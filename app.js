bccb1000ae5c637115923f7790c8fd0c78a5666b
Rafael Monteiro <rafaell.elias@hotmail.com>
2022-02-06 17:58:46 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Aprendendo criar uma pipeline na AWS uhuuuull!'));
+app.get('/', (req, res) => res.send('Aprendendo a criar um novo estágio para revisar o código uhuuuul!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
