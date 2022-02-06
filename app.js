6b29fc38adb4c7233228c49baef00dbe9aa9702d
Rafael Monteiro <rafaell.elias@hotmail.com>
2022-02-06 18:05:46 -0300

    testando

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Aprendendo a criar um novo estágio para revisar o código uhuuuul!'));
+app.get('/', (req, res) => res.send('Qualquer alteração q eu fizer agora vai direto uhuul!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
